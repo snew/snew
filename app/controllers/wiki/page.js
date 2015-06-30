942c8810bdd7cbde1482d165e0fd4e5f9eaf2a45
go1dfish <justageekbiz@gmail.com>
2015-06-30 12:04:04 -0700

    wiki improvements

--- app/controllers/wiki/page.js
+++ app/controllers/wiki/page.js
@@ -13,19 +13,33 @@ export default Ember.Controller.extend({
     }).filter(function(url) {
       return !!url.match(/reddit.com\/r\/.*\/comments/i);
     }).map(function(url) {
-      var path = url.split('reddit.com/', 2)[1];
+      var path = (url.split('reddit.com/', 2)[1] || '').split('?')[0];
       var parts = path.split('/').without('');
-      if (parts.length > 5) {
+      if (parts.length > 5 && parts[5].length === 7) {
         return 't1_' + parts[5];
       }
+      if ((parts[3] || '').length !== 6) {return '';}
       return 't3_' + parts[3];
-    });
+    }).without('').uniq().sort().reverse();
   }.property('model.content_html'),
+
+  batches: function() {
+    var ids = this.get('ids').slice();
+    var batches = [];
+    while (ids.length) {batches.push(ids.splice(0, 100));}
+    return batches;
+  }.property('ids'),
 
   idsDidChange: function() {
     var ids = this.get('ids').slice();
-    while (ids.length) {this.fetchBatch(ids.splice(0, 100))}
-  }.observes('ids').on('init'),
+    this.get('batches').forEach(function(ids) {
+      this.fetchBatch(ids)
+    }.bind(this));
+  }.observes('batches').on('init'),
+
+  links: Ember.computed.map('batches', function(ids) {
+    return 'https://us.reddit.com/api/info?id=' + ids.join(',');
+  }),
 
   fetchBatch: function(ids) {
     return this.get('snoocore.client')('/api/info.json').listing({
