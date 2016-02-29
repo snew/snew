2fe4995325ae17bb31f7930506534e22d57b7f59
go1dfish <justageekbiz@gmail.com>
2016-02-29 12:37:52 -0800

    Include updated scores, text and removal status in comment search results

--- app/routes/comment-search.js
+++ app/routes/comment-search.js
@@ -1,5 +1,6 @@
 import Ember from 'ember';
 import ListingRouteMixin from 'snew/mixins/listing-route';
+import {fetchIds} from 'snew/services/snoocore';
 
 export default Ember.Route.extend(ListingRouteMixin, {
   queryParams: {
@@ -8,6 +9,8 @@ export default Ember.Route.extend(ListingRouteMixin, {
   },
 
   model: function(params) {
+    const client = this.get('snoocore.client');
+
     if (!params.q) {return [];}
     return Ember.RSVP.resolve(Ember.$.ajax({
       url: "https://api.pushshift.io/reddit/search?" + Ember.$.param(params)
@@ -15,6 +18,26 @@ export default Ember.Route.extend(ListingRouteMixin, {
       return (data.data || []).map(function(item) {
         item.body_html = $('<textarea />').html(item.body_html).text();
         return item;
+      });
+    }).then(results => {
+      return fetchIds(client, results.map(c => 't1_' + c.id)).then(fromReddit => {
+        fromReddit.forEach(redditComment => {
+          const comment = results.findBy('id', redditComment.id);
+
+          if (redditComment.body === '[removed]') {
+            delete redditComment.body;
+            delete redditComment.body_html;
+            delete redditComment.author;
+            redditComment.banned_by = true;
+          }
+
+          Ember.setProperties(comment, redditComment);
+        });
+
+        return results.filter(c => c.body !== '[deleted]');
+      }).catch(error => {
+        console.error('Error fetching from reddit', error.stack || error);
+        return results;
       });
     });
   }
