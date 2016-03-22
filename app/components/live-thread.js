77f957b458160c8b9067db439b9b98625b7aa0e4
go1dfish <justageekbiz@gmail.com>
2016-03-22 02:27:08 -0700

    Live thread fix

--- app/components/live-thread.js
+++ app/components/live-thread.js
@@ -13,13 +13,11 @@ export default Ember.Component.extend(LiveSocketMixin, {
     return post;
   }),
 
-  didReceiveSocketEvent: function(data) {
+  receiveSocketEvent: function(data) {
     try {
       var bot = this.get('snoocore.bot');
       console.log('data', data);
-      var post = JSON.parse(data.body.trim());
-      console.log('post', post);
-      this.get('updates').insertAt(0, post);
+      this.get('updates').insertAt(0, data);
     } catch(e) {console.error(e);}
   }.on('didReceiveSocketEvent'),
 
